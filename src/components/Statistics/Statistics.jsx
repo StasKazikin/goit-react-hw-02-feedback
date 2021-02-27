5fed859e559818cf660eca80193ac2175b75584f
Stanislav Kazikin <kazikinstas@gmail.com>
2021-02-28 01:07:02 +0200

    блок статистики рендерится только после того, как был собран хотя бы один отзыв

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,21 @@
+import Notification from '../Notification/Notification';
+
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
   <>
     <h2>Statistics</h2>
-    <ul>
-      <li>Good: {good}</li>
-      <li>Neutral: {neutral}</li>
-      <li>Bad: {bad}</li>
-      <li>Total: {total}</li>
-      <li>Positive feedback: {positivePercentage ? positivePercentage : 0}%</li>
-    </ul>
+    {total === 0 ? (
+      <Notification message="No feedback given" />
+    ) : (
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>Total: {total}</li>
+        <li>
+          Positive feedback: {positivePercentage ? positivePercentage : 0}%
+        </li>
+      </ul>
+    )}
   </>
 );
 
